c139814bd4f52b943b6470a3393f507f2e3e64de
Wesley Moore <wez@wezm.net>
2009-03-26 15:02:18 +1100

    Fix sleep duration and path to ruby interpreter

--- skype_pounce.rb
+++ skype_pounce.rb
@@ -1,14 +1,15 @@
-#!/bin/ruby
+#!/usr/bin/ruby
 
 if ARGV.size < 1
-	puts "Usage: skype_pounce.rb <username>"
+	puts "Usage: skype_pounce <username>"
 	exit 2
 end
 
 user = ARGV[0]
 user_status='UNKNOWN'
 while user_status != 'ONLINE'
-	sleep 1
+	sleep 30
+	# XXX You will need to change this path if you are not wmoore
 	user_status = `osascript "/Users/wmoore/Source/get_skype_user_status.scpt" "#{user}"`
 	user_status = user_status.split[-1]
 end
